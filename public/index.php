e5ac0996853a140c4c5e923a333074ab9eb0427f
dannycornelio <licdannycornelio@gmail.com>
2023-08-05 11:15:12 -0600

    correccion del mantenimineto de materias

--- public/index.php
+++ public/index.php
@@ -15,12 +15,12 @@ $router->get('/alumnos', [AlumnoController::class,'index'] );
 $router->get('/materias', [MateriaController::class,'index'] );
 $router->post('/API/alumnos/guardar', [AlumnoController::class,'guardarAPI'] );
 $router->post('/API/materias/guardar', [MateriaController::class,'guardarAPI'] );
+$router->get('/API/alumnos/buscar', [AlumnoController::class,'buscarAPI'] );
+$router->get('/API/materias/buscar', [MateriaController::class,'buscarAPI'] );
 $router->post('/API/alumnos/modificar', [AlumnoController::class,'modificarAPI'] );
 $router->post('/API/materias/modificar', [MateriaController::class,'modificarAPI'] );
 $router->post('/API/alumnos/eliminar', [AlumnoController::class,'eliminarAPI'] );
 $router->post('/API/materias/eliminar', [MateriaController::class,'eliminarAPI'] );
-$router->get('/API/alumnos/buscar', [AlumnoController::class,'buscarAPI'] );
-$router->get('/API/materias/buscar', [MateriaController::class,'buscarAPI'] );
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
