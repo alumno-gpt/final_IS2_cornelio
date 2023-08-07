eb916b170144c675d4980e02334f4a62044a142a
dannycornelio <licdannycornelio@gmail.com>
2023-08-07 17:39:16 -0600

    se le dio formato al codigo

--- controllers/MateriaController.php
+++ controllers/MateriaController.php
@@ -6,26 +6,29 @@ use Exception;
 use Model\Materia;
 use MVC\Router;
 
-class MateriaController{
-    public static function index(Router $router){
+class MateriaController
+{
+    public static function index(Router $router)
+    {
         $materias = Materia::all();
         $router->render('materias/index', [
             'materias' => $materias,
-      ]);
+        ]);
 
     }
 
-    public static function guardarAPI(){
+    public static function guardarAPI()
+    {
         try {
             $materia = new Materia($_POST);
             $resultado = $materia->crear();
 
-            if($resultado['resultado'] == 1){
+            if ($resultado['resultado'] == 1) {
                 echo json_encode([
                     'mensaje' => 'Registro guardado correctamente',
                     'codigo' => 1
                 ]);
-            }else{
+            } else {
                 echo json_encode([
                     'mensaje' => 'Ocurrió un error',
                     'codigo' => 0
@@ -41,17 +44,18 @@ class MateriaController{
         }
     }
 
-    public static function modificarAPI(){
+    public static function modificarAPI()
+    {
         try {
             $materia = new Materia($_POST);
             $resultado = $materia->actualizar();
 
-            if($resultado['resultado'] == 1){
+            if ($resultado['resultado'] == 1) {
                 echo json_encode([
                     'mensaje' => 'Registro modificado correctamente',
                     'codigo' => 1
                 ]);
-            }else{
+            } else {
                 echo json_encode([
                     'mensaje' => 'Ocurrió un error',
                     'codigo' => 0
@@ -67,19 +71,20 @@ class MateriaController{
         }
     }
 
-    public static function eliminarAPI(){
+    public static function eliminarAPI()
+    {
         try {
             $id_materias = $_POST['id_materias'];
             $materia = Materia::find($id_materias);
-            $materia-> detalle_situacion= 0;
+            $materia->detalle_situacion = 0;
             $resultado = $materia->actualizar();
 
-            if($resultado['resultado'] == 1){
+            if ($resultado['resultado'] == 1) {
                 echo json_encode([
                     'mensaje' => 'Registro eliminado correctamente',
                     'codigo' => 1
                 ]);
-            }else{
+            } else {
                 echo json_encode([
                     'mensaje' => 'Ocurrió un error',
                     'codigo' => 0
@@ -95,17 +100,18 @@ class MateriaController{
         }
     }
 
-    public static function buscarAPI(){
+    public static function buscarAPI()
+    {
         $ma_nombre = $_GET['ma_nombre'];
 
         $sql = "SELECT * FROM materias where detalle_situacion = 1 ";
-        if($ma_nombre != '') {
-            $sql.= " and ma_nombre like '%$ma_nombre%' ";
+        if ($ma_nombre != '') {
+            $sql .= " and ma_nombre like '%$ma_nombre%' ";
         }
         try {
-            
+
             $materias = Materia::fetchArray($sql);
-    
+
             echo json_encode($materias);
         } catch (Exception $e) {
             echo json_encode([
--- models/Calificacion.php
+++ models/Calificacion.php
@@ -2,9 +2,10 @@
 
 namespace Model;
 
-class Calificacion extends ActiveRecord{
+class Calificacion extends ActiveRecord
+{
     public static $tabla = 'calificaciones';
-    public static $columnasDB = ['calif_alumno','calif_materia','calif_punteo','calif_resultado','detalle_situacion'];
+    public static $columnasDB = ['calif_alumno', 'calif_materia', 'calif_punteo', 'calif_resultado', 'detalle_situacion'];
     public static $idTabla = 'id_calificaciones';
 
     public $id_calificaciones;
@@ -14,7 +15,7 @@ class Calificacion extends ActiveRecord{
     public $calif_resultado;
     public $detalle_situacion;
 
-    public function __construct($args = [] )
+    public function __construct($args = [])
     {
         $this->id_calificaciones = $args['id_calificaciones'] ?? null;
         $this->calif_alumno = $args['calif_alumno'] ?? '';
@@ -33,18 +34,19 @@ class Calificacion extends ActiveRecord{
         INNER JOIN alumnos ON calif_alumno = id_alumnos
         WHERE calificaciones.detalle_situacion = 1 ";
 
-        if($this->calif_alumno != '') {
-            $sql.= " and calif_alumno = '$this->calif_alumno' ";
+        if ($this->calif_alumno != '') {
+            $sql .= " and calif_alumno = '$this->calif_alumno' ";
         }
-        if($this->calif_materia != '') {
-            $sql.= " and calif_materia = '$this->calif_materia' ";
+        if ($this->calif_materia != '') {
+            $sql .= " and calif_materia = '$this->calif_materia' ";
         }
- 
+
         return self::fetchArray($sql);
     }
 
-    public function eliminarCalificacion() {
-        $query = "UPDATE "  . static::$tabla . " SET detalle_situacion = 0 WHERE id_calificaciones = " . self::$db->quote($this->id_calificaciones);
+    public function eliminarCalificacion()
+    {
+        $query = "UPDATE " . static::$tabla . " SET detalle_situacion = 0 WHERE id_calificaciones = " . self::$db->quote($this->id_calificaciones);
         $resultado = self::$db->exec($query);
         return [
             'resultado' => $resultado,
