a1d38cbccabbd3a63ae42753cd425584a1298e42
dannycornelio <licdannycornelio@gmail.com>
2023-08-06 19:02:34 -0600

    el mantenimiento de calificaciones ya muestra

--- views/materias/index.php
+++ views/materias/index.php
@@ -1,12 +1,27 @@
-<h1 class="text-center">Formulario de materias</h1>
+<h1 class="text-center">Formulario de calificaciones</h1>
 <div class="row justify-content-center mb-5">
     <form class="col-lg-8 border bg-light p-3" id="formularioMaterias">
-        <input type="hidden" name="id_materias" id="id_materias">
+        <input type="hidden" name="id_calificaciones" id="id_calificaciones">
         <div class="row mb-3">
-            <div class="col">
-                <label for="ma_nombre">Nombre de la materia</label>
-                <input type="text" name="ma_nombre" id="ma_nombre" class="form-control">
-            </div>
+        <select name="calif_alumno" id="calif_alumno" class="form-control">
+            <option value="">SELECCIONE...</option>
+                <?php foreach ($alumnos as $key => $alumno) : ?>
+                <option value="<?= $alumno['ID_ALUMNOS'] ?>"><?= $alumno['ALU_NOMBRE'] . ' ' . $alumno['ALU_APELLIDO'] ?></option>
+                <?php endforeach ?>
+            </select>
+        </div>
+        <div class="col-lg-8">
+            <label for="materia1">Materia</label>
+            <select name="calif_materia" id="materia1" class="form-control">
+                <option value="">SELECCIONE...</option>
+                <?php foreach ($materias as $key => $materia) : ?>
+                <option value="<?= $materia['ID_MATERIAS'] ?>"><?= $materia['MA_NOMBRE'] ?></option>
+                <?php endforeach ?>
+            </select>
+        </div>
+        <div class="col-lg-4">
+            <label for="calificacion1">Punteo</label>
+            <input type="number" step="any" name="calif_punteo" id="calificacion1" class="form-control" placeholder="ejemplo: 75.50">
         </div>
         
         <div class="row mb-3">
